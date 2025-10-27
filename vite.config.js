8d8b3a2bb56afce2d7948dffd14628748013646a
Astral-07 <93427669+Astral-07@users.noreply.github.com>
2025-10-27 17:47:31 +0530

    Update vite.config.js
    Fixed "base:" again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/caffeine-tracker/",
+  base: process.env.VITE_BASE_PATH || "/caffeine-tracker/",
   plugins: [react()],
 })
